8b636f8e1cabac14d5f37b93326100e5aecb9bfe
Siarhei Mitasau <darkenRahl1893@gmail.com>
2019-03-24 22:45:49 +0300

    219ms

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let open = [];
+  let close = [];
+
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    open[i] = bracketsConfig[i][0];
+    close[i] = bracketsConfig[i][1];
+  }
+
+  let arr = str.split('');
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let index in open) {
+      if (arr[i] == open[index]) {
+        if (arr[i + 1] == close[index]) {
+          arr.splice(i, 2);
+          console.log(arr);
+          i -= i + 1;
+        }
+      }
+    }
+  }
+  if (arr.length !== 0) {
+    return false;
+  } else {
+    return true;
+  }
+};
